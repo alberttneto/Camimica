332de078afc641ca4e1ed015b35e1e81548db0b4
ALBERTO FERREIRA NETO <alberto.neto@martins.com.br>
2024-02-08 13:39:47 -0300

    Definição times, alterado para input range

--- js/qtdTimes.js
+++ js/qtdTimes.js
@@ -1,6 +1,17 @@
 var qtdTimes = 2;
 var qtdPessoas = 2;
 
+const span = document.querySelectorAll("span");
+console.log(span[0].nextElementSibling);
+
+
+span.forEach((element) => {
+    element.nextElementSibling.addEventListener('change', () => {
+        element.innerHTML = element.nextElementSibling.value;
+    });
+});
+
+
 function buscaQtds() {
     qtdTimes = document.getElementsByTagName("input")[0].value;
     qtdPessoas = document.getElementsByTagName("input")[1].value;
